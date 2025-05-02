3a9110882f5b5d2fc8a3a25819b4ac11696ea3b9
krish542 <krishnakapadia910@gmail.com>
2025-05-02 15:20:28 +0530

    deploy trial

--- frontend/src/utils/apiConfig.js
+++ frontend/src/utils/apiConfig.js
@@ -1,6 +1,6 @@
 // frontend/src/utils/apiConfig.js
 const API_BASE_URL_DEV = 'http://localhost:5000'; // Default development URL
-const API_BASE_URL_PROD = 'your-production-backend-url.com';
+const API_BASE_URL_PROD = 'https://learniverse-0ow7.onrender.com';
 
 const API_BASE_URL = process.env.NODE_ENV === 'development'
   ? API_BASE_URL_DEV
